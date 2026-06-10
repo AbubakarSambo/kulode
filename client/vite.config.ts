f515f56d6ab5b41206253246a992cd0430fa3e4b
AbubakarSambo <abubakar.sambo@mail.mcgill.ca>
2026-06-10 17:28:18 +0100

    vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     host: true, // Allow access from network
     proxy: {
       '/api': {
-        target: 'http://localhost:3003',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
